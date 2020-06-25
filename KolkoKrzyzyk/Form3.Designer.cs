1256b4529c0e90801635fe5ef6f198303298f9e5
Your Name <pstruzik7@wp.pl>
2020-06-25 19:22:27 +0200

    Dodanie drugiej klasy

--- KolkoKrzyzyk/Form3.Designer.cs
+++ KolkoKrzyzyk/Form3.Designer.cs
@@ -37,6 +37,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button7
@@ -120,11 +122,31 @@
             this.button1.TabIndex = 18;
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Kolej gracza:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "o";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 436);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button9);
@@ -137,6 +159,7 @@
             this.Name = "Form3";
             this.Text = "Człowiek vs Komputer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,5 +174,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
---
+++ TicTacToeLogic/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace TicTacToeLogic
+{
+    class Class2
+    {
+    }
+}
